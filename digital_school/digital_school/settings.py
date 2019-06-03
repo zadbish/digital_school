49f51cd1553708be449b1c82a6a9e4d40982ca94
zahid <zadbish@gmail.com>
2019-06-03 23:42:02 +0500

    blog profle completed

--- digital_school/digital_school/settings.py
+++ digital_school/digital_school/settings.py
@@ -147,6 +147,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 TENANT_MODEL = "customers.Client"  # app.Model
 
 TENANT_DOMAIN_MODEL = "customers.Domain"  # app.Model
